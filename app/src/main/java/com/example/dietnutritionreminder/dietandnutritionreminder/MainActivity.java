66dad00667c77ed680f0a538aa69bcb9fe514f7f
afterburner07 <afterburning.turbo726@gmail.com>
2019-05-23 17:16:31 +0600

    Added Second Activity

--- app/src/main/java/com/example/dietnutritionreminder/dietandnutritionreminder/MainActivity.java
+++ app/src/main/java/com/example/dietnutritionreminder/dietandnutritionreminder/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.dietnutritionreminder.dietandnutritionreminder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     void changeScreen()
     {
         ;
+    }
+
+    public void buttonForwardOnClick(View v)
+    {
+        Intent secondIntent=new Intent(this,SecondActivity.class);
+        startActivity(secondIntent);
+    }
+
+    public void buttonExitOnClick(View v)
+    {
+        finish();
+        System.exit(0);
     }
 }
