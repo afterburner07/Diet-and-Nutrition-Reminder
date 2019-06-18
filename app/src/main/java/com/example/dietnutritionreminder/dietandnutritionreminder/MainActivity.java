8ae105eafe48ab9764f8978b9d20e21b619d943f
afterburner07 <afterburning.turbo726@gmail.com>
2019-06-19 04:29:35 +0600

    made splash screen correctly. Made total nutrition meter display.

--- app/src/main/java/com/example/dietnutritionreminder/dietandnutritionreminder/MainActivity.java
+++ app/src/main/java/com/example/dietnutritionreminder/dietandnutritionreminder/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.dietnutritionreminder.dietandnutritionreminder;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                nextActivity();
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
     }
     void changeScreen()
     {
@@ -18,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buttonForwardOnClick(View v)
+    {
+        nextActivity();
+    }
+
+    public void nextActivity()
     {
         Intent secondIntent=new Intent(this,SecondActivity.class);
         startActivity(secondIntent);
