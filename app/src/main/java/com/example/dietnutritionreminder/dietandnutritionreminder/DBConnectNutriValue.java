210e11b31c3d0af93a01b890f0ae56655b6ea83c
monika-masum <monika2masum@gmail.com>
2019-07-01 18:00:11 +0600

    implementated new app icon, added server database initial class, new settings activity using floating button(settings icon) on main activity.

---
+++ app/src/main/java/com/example/dietnutritionreminder/dietandnutritionreminder/DBConnectNutriValue.java
@@ -0,0 +1,43 @@
+package com.example.dietnutritionreminder.dietandnutritionreminder;
+
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnectNutriValue {
+    // String ip = "192.168.1.10";
+    String ip = "192.168.0.106";
+    String classs = "net.sourceforge.jtds.jdbc.Driver";
+    String db = "Andro";
+    String un = "RumkyApu";
+    String password = "123";
+
+    @SuppressLint("NewApi")
+    public Connection CONN() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                .permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection conn = null;
+        String ConnURL = null;
+        try {
+
+            Class.forName(classs);
+            ConnURL = "jdbc:jtds:sqlserver://" + ip + ";"
+                    + "databaseName=" + db + ";user=" + un + ";password="
+                    + password + ";";
+            conn = DriverManager.getConnection(ConnURL);
+        } catch (SQLException se) {
+            Log.e("ERRO", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("ERRO", e.getMessage());
+        } catch (Exception e) {
+            Log.e("ERRO", e.getMessage());
+        }
+        return conn;
+    }
+
+}
+
