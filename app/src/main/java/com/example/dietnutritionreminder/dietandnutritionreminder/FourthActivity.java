5aca2d124f3d65337adf302bdb8616d8db42d82c
shilpi-sarkar <shilpisarkar1777@gmail.com>
2019-07-11 21:17:33 +0600

    meal list and food list names dynamically update initially done.

--- app/src/main/java/com/example/dietnutritionreminder/dietandnutritionreminder/FourthActivity.java
+++ app/src/main/java/com/example/dietnutritionreminder/dietandnutritionreminder/FourthActivity.java
@@ -4,13 +4,44 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class FourthActivity extends AppCompatActivity {
+
+    TextView textViewFoodName;
+    public String foodName=new String();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fourth);
+        foodName=getIntent().getStringExtra("FoodName");
+        textViewFoodName=(TextView) findViewById(R.id.textViewfoodName);
+        textViewFoodName.setText(foodNameCheck());
+    }
+
+    public String foodNameCheck() {
+        String returnMealName = new String();
+        switch (foodName) {
+            case "ch":
+                returnMealName = "Chicken";
+                break;
+            case "ri":
+                returnMealName = "Rice";
+                break;
+            case "fr":
+                returnMealName = "Fruits";
+                break;
+            case "ve":
+                returnMealName = "Vegetable";
+                break;
+            case "fi":
+                returnMealName = "Fish";
+                break;
+            default:
+                returnMealName = "Chicken";
+        }
+        return returnMealName;
     }
 
     public void onClickForward(View v)
--- app/src/main/java/com/example/dietnutritionreminder/dietandnutritionreminder/ThirdActivity.java
+++ app/src/main/java/com/example/dietnutritionreminder/dietandnutritionreminder/ThirdActivity.java
@@ -4,21 +4,69 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ThirdActivity extends AppCompatActivity {
+
+    TextView textViewMealName;
+    public char mealName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+        mealName=getIntent().getCharExtra("MealName",'l');
+        textViewMealName=(TextView) findViewById(R.id.textViewMealName);
+        textViewMealName.setText(mealNameCheck());
+    }
+
+    public String mealNameCheck() {
+        String returnMealName=new String();
+        switch (mealName)
+        {
+            case 'b':
+                returnMealName="Breakfast";
+                break;
+            case 'l':
+                returnMealName="Lunch";
+                break;
+            case 'a':
+                returnMealName="Afternoon";
+                break;
+            case 'd':
+                returnMealName="Dinner";
+                break;
+            default:
+                returnMealName="Lunch";
+        }
+        return returnMealName;
     }
 
     public void onClicktextChicken(View v) {
-        gotoNutritionChicken();
+        gotoNutritionChicken("ch");
     }
 
-    private void gotoNutritionChicken() {
+    public void onClicktextRice(View v) {
+        gotoNutritionChicken("ri");
+    }
+
+    public void onClicktextFruits(View v) {
+        gotoNutritionChicken("fr");
+    }
+    public void onClicktextVegetable(View v) {
+        gotoNutritionChicken("ve");
+    }
+
+    public void onClicktextFish(View v) {
+        gotoNutritionChicken("fi");
+    }
+
+    private void gotoNutritionChicken(String selectedFood) {
         Intent intentFourth=new Intent(this,FourthActivity.class);
+        intentFourth.putExtra("FoodName",selectedFood );
         startActivity(intentFourth);
     }
 
@@ -33,6 +81,6 @@ public class ThirdActivity extends AppCompatActivity {
 
     public void onClickForward(View v)
     {
-        gotoNutritionChicken();
+        gotoNutritionChicken("ch");
     }
 }
